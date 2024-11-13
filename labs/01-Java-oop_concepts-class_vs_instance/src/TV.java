ffd71a0baff5940f46c7b725d354f1c6e1dd5082
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2024-11-13 13:31:09 +0100

    Implement exercise 1 for week 7, day 3

--- labs/01-Java-oop_concepts-class_vs_instance/src/TV.java
+++ labs/01-Java-oop_concepts-class_vs_instance/src/TV.java
@@ -1,65 +1,161 @@
-public class TV {
-    int channel;
-    int volumeLevel;
-    boolean turnedOn;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
-    /**
-     * The constructor. Called when you instantiate a TV.
-     */
-    public TV() {
-    }
+public class TV {
+    private int channel = 1;
+    private final ArrayList<Integer> channels = new ArrayList<>(
+        IntStream.rangeClosed(1, 100).boxed().toList()
+    );
+    private int volumeLevel = 1;
+    private boolean turnedOn = false;
 
     /**
      * Turns the TV on.
      */
-    public void turnOn() {
+    public TV turnOn() {
+        this.turnedOn = true;
 
+        return this;
     }
 
     /**
      * Turns the TV off.
      */
-    public void turnOff() {
+    public TV turnOff() {
+        this.turnedOn = false;
 
+        return this;
     }
 
     /**
      * Scrolls the channel up.
      */
     public void channelUp() {
+        if (this.channel < 1) {
+            throw new IllegalStateException("The channel cannot be less than 1.");
+        }
 
+        this
+            .isTurnedOn()
+            .channel++
+        ;
     }
 
     /**
      * Scrolls the channel down.
      */
     public void channelDown() {
-
+        this
+            .isChannelValid(this.channel)
+            .channel--
+        ;
     }
 
     /**
      * Sets the channel to the desired channel.
      * @param newChannel the new channel.
      */
-    public void setChannel(int newChannel) {
+    public TV setChannel(int newChannel) {
+        this
+            .isChannelValid(newChannel)
+            .channel = newChannel
+        ;
 
+        return this;
+    }
+
+    /**
+     * Sets the volume to the desired volume level.
+     * @param newVolumeLevel the new volume level.
+     * @return the TV object.
+     */
+    public TV setVolume(int newVolumeLevel) {
+        this
+            .isTurnedOn()
+            .volumeLevel = newVolumeLevel
+        ;
+
+        return this;
     }
 
     /**
      * Increases the volume.
      */
     public void volumeUp() {
-
+        this
+            .isTurnedOn()
+            .volumeLevel++
+        ;
     }
 
     /**
      * Decreases the volume.
      */
     public void volumeDown() {
-
+        this
+            .isTurnedOn()
+            .volumeLevel--
+        ;
     }
 
-    public String isOn() {
-        return turnedOn ? "on" : "off";
+    /**
+     * Gets the volume level.
+     * @return the volume level.
+     */
+    public int getChannel() {
+        return this.channel;
+    }
+
+    /**
+     * Gets the volume level.
+     * @return the volume level.
+     */
+    public int getVolumeLevel() {
+        return this.volumeLevel;
+    }
+
+    /**
+     * Checks if the TV is on.
+     * @return true if the TV is on, false otherwise.
+     */
+    public boolean isOn() {
+        return this.turnedOn;
+    }
+
+    /**
+     * Gets the TV status.
+     * @return the TV status.
+     */
+    public String isOnAsString() {
+        return this.turnedOn ? "on" : "off";
+    }
+
+    /**
+     * Checks if the TV is on.
+     * @return the TV object.
+     * @throws IllegalStateException if the TV is off.
+     */
+    private TV isTurnedOn() {
+        if (!this.turnedOn) {
+            throw new IllegalStateException("The TV is off. Turn it on first.");
+        }
+
+        return this;
+    }
+
+    /**
+     * Checks if the channel is valid.
+     * @param channel the channel to check.
+     * @return the TV object.
+     * @throws IllegalStateException if the channel is out of bounds.
+     */
+    private TV isChannelValid(int channel) {
+        this.isTurnedOn();
+
+        if (!this.channels.contains(channel)) {
+            throw new IllegalStateException(String.format("The channel [%d] is out of bounds.", channel));
+        }
+
+        return this;
     }
 }
--- labs/01-Java-oop_concepts-class_vs_instance/src/TVParty.java
+++ labs/01-Java-oop_concepts-class_vs_instance/src/TVParty.java
@@ -2,34 +2,32 @@
  * Let's make a TV party!!
  */
 public class TVParty {
-
     /**
      * Main method to the TV party.
      * @param args args
      */
     public static void main(String[] args) {
-        //TODO instantiate the TV object
-        TV tv = null;
+        TV tv = new TV();
 
-        //TODO Your first code goes here
+        tv
+            .turnOn()
+            .setChannel(3)
+            .setVolume(7)
+        ;
+        System.out.println("Let's watch the Alien Movie. The TV is currently [" + tv.isOnAsString() + "] and it should be [on].");
+        System.out.println("It's being shown on channel [3], and we're currently on channel [" + tv.getChannel() + "].");
+        System.out.println("Your friend Lisa also would like to have the volume set to [7], and we're currently on volume [" + tv.getVolumeLevel() + "].");
 
-        System.out.println("Let's watch the Alien Movie. The TV is currently [" + tv.isOn() + "] and it should be [on]." +
-                " It's being shown on channel [3], and we're currently on channel [" + tv.channel + "]. " +
-                "Your friend Lisa also would like to have the volume set to [7], and we're currently on volume [" + tv.volumeLevel + "].");
+        tv.turnOff();
+        System.out.println("Food break! The TV should be [off], and it's currently [" + tv.isOnAsString() + "].");
 
-        //TODO food break! turn the tv off.
-
-        System.out.println("Food break! The TV should be [off], and it's currently [" + tv.isOn() + "].");
-
-        //TODO your second code goes here
-
-        System.out.println("Now let's watch the last season of Game of Thrones. The TV is currently [" + tv.isOn() +
-                "] and it should be [on]. " +
-                "It's being shown on channel [95], and we're currently on channel [" + tv.channel + "]. " +
-                "Your friend Gabriel also would like to have the volume set to [5], and we're currently on volume [" + tv.volumeLevel + "].");
+        tv
+            .turnOn()
+            .setChannel(95)
+            .setVolume(5)
+        ;
+        System.out.println("Now let's watch the last season of Game of Thrones. The TV is currently [" + tv.isOnAsString() + "] and it should be [on].");
+        System.out.println("It's being shown on channel [95], and we're currently on channel [" + tv.getChannel() + "].");
+        System.out.println("Your friend Gabriel also would like to have the volume set to [5], and we're currently on volume [" + tv.getVolumeLevel() + "].");
     }
-
-
-
-
 }
