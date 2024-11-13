e36680d728de48c51b70d69c6e766745211f6c1a
maryam <maryam.ghayoomi@gmail.com>
2024-11-13 13:23:12 +0100

    Add solutions for 01-Java-oop_concepts-class_vs_instance project

--- labs/01-Java-oop_concepts-class_vs_instance/src/TV.java
+++ labs/01-Java-oop_concepts-class_vs_instance/src/TV.java
@@ -7,34 +7,41 @@ public class TV {
      * The constructor. Called when you instantiate a TV.
      */
     public TV() {
+        channel = 1;
+        volumeLevel = 1;
+        turnedOn = false;
     }
 
     /**
      * Turns the TV on.
      */
     public void turnOn() {
-
+        turnedOn = true;
     }
 
     /**
      * Turns the TV off.
      */
     public void turnOff() {
-
+        turnedOn = false;
     }
 
     /**
      * Scrolls the channel up.
      */
     public void channelUp() {
-
+        if (turnedOn && channel < 100) {
+            channel++;
+        }
     }
 
     /**
      * Scrolls the channel down.
      */
     public void channelDown() {
-
+        if (turnedOn && channel > 1) {
+            channel--;
+        }
     }
 
     /**
@@ -42,21 +49,27 @@ public class TV {
      * @param newChannel the new channel.
      */
     public void setChannel(int newChannel) {
-
+        if (turnedOn && newChannel >= 1 && newChannel<= 100) {
+            channel = newChannel;
+        }
     }
 
     /**
      * Increases the volume.
      */
     public void volumeUp() {
-
+        if (turnedOn && volumeLevel < 10) {
+            volumeLevel++;
+        }
     }
 
     /**
      * Decreases the volume.
      */
     public void volumeDown() {
-
+        if (turnedOn && volumeLevel > 1) {
+            volumeLevel--;
+        }
     }
 
     public String isOn() {
