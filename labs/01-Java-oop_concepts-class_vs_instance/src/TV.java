891a264a082e0e611fdfd8618109332b9d8c88b9
Hasan <5iveatc@gmail.com>
2024-11-14 08:48:14 +0100

    tv task

--- labs/01-Java-oop_concepts-class_vs_instance/src/TV.java
+++ labs/01-Java-oop_concepts-class_vs_instance/src/TV.java
@@ -1,26 +1,32 @@
 public class TV {
     int channel;
     int volumeLevel;
-    boolean turnedOn;
+    boolean turnedOn= false;
 
     /**
      * The constructor. Called when you instantiate a TV.
      */
-    public TV() {
+    public TV(int channel, int volumeLevel) {
+
+        this.channel= channel;
+        this.volumeLevel= volumeLevel;
+        System.out.println("The TV is There");
     }
 
     /**
      * Turns the TV on.
      */
     public void turnOn() {
-
+        this.turnedOn= true;
+        System.out.println("The Tv is now on");
     }
 
     /**
      * Turns the TV off.
      */
     public void turnOff() {
-
+        this.turnedOn= false;
+        System.out.println("The Tv is now off");
     }
 
     /**
@@ -28,13 +34,24 @@ public class TV {
      */
     public void channelUp() {
 
+        if ( turnedOn ){
+            if ( channel <100 ){
+                channel +=1 ;
+                System.out.println("Channel changed to "+ channel + " .");
+            }
+        }
     }
 
     /**
      * Scrolls the channel down.
      */
     public void channelDown() {
-
+        if ( turnedOn ){
+            if(channel >1 ){
+                channel -=1 ;
+                System.out.println("Channel changed to "+ channel + " .");
+            }
+        }
     }
 
     /**
@@ -42,21 +59,34 @@ public class TV {
      * @param newChannel the new channel.
      */
     public void setChannel(int newChannel) {
-
+        if ( 0<newChannel && newChannel <=100 ){
+            this.channel= newChannel;
+            System.out.println("Channel changed to "+ newChannel + " .");
+        }
     }
 
     /**
      * Increases the volume.
      */
     public void volumeUp() {
-
+        if ( turnedOn ){
+            if (volumeLevel <10 ){
+                volumeLevel +=1 ;
+                System.out.println("VolumeLevel changed to "+ volumeLevel + " .");
+            }
+        }
     }
 
     /**
      * Decreases the volume.
      */
     public void volumeDown() {
-
+        if ( turnedOn ){
+            if ( volumeLevel >1 ){
+                volumeLevel -=1 ;
+                System.out.println("VolumeLevel changed to "+ volumeLevel + " .");
+            }
+        }
     }
 
     public String isOn() {
--- labs/01-Java-oop_concepts-class_vs_instance/src/TVParty.java
+++ labs/01-Java-oop_concepts-class_vs_instance/src/TVParty.java
@@ -9,17 +9,24 @@ public class TVParty {
      */
     public static void main(String[] args) {
         //TODO instantiate the TV object
-        TV tv = null;
+        TV tv = new TV(1,1);
 
         //TODO Your first code goes here
 
         System.out.println("Let's watch the Alien Movie. The TV is currently [" + tv.isOn() + "] and it should be [on]." +
                 " It's being shown on channel [3], and we're currently on channel [" + tv.channel + "]. " +
                 "Your friend Lisa also would like to have the volume set to [7], and we're currently on volume [" + tv.volumeLevel + "].");
-
+        tv.turnOn();
+        tv.setChannel(3);
+        tv.volumeUp();
+        tv.volumeUp();
+        tv.volumeUp();
+        tv.volumeUp();
+        tv.volumeUp();
+        tv.volumeUp();
         //TODO food break! turn the tv off.
-
         System.out.println("Food break! The TV should be [off], and it's currently [" + tv.isOn() + "].");
+        tv.turnOff();
 
         //TODO your second code goes here
 
@@ -27,6 +34,10 @@ public class TVParty {
                 "] and it should be [on]. " +
                 "It's being shown on channel [95], and we're currently on channel [" + tv.channel + "]. " +
                 "Your friend Gabriel also would like to have the volume set to [5], and we're currently on volume [" + tv.volumeLevel + "].");
+        tv.turnOn();
+        tv.setChannel(95);
+        tv.volumeDown();
+        tv.volumeDown();
     }
 
 
